30e7748fa361f6a4715c89bdfda8fbe6efcb82b2
Navaneeth Rao <navaneethbv@gmail.com>
2015-01-10 20:38:29 -0600

    Client_Server_FTP

---
+++ FtpServer.java
@@ -0,0 +1,154 @@
+//FtpServer.java
+import java.io.*;
+import java.net.*;
+
+public class FtpServer
+{
+     public static void main(String [] args)
+     {
+          int i=1;
+
+System.out.println("******************************************************
+**************************");
+          System.out.println("******************************   FTP 
+SERVER
+***********************************");
+
+System.out.println("******************************************************
+**************************");
+          System.out.println("Server Started...");
+          System.out.println("Waiting for connections...");
+          System.out.println("
+");
+          try
+          {
+
+               ServerSocket s = new ServerSocket(100);
+               for(;;)
+               {
+                    Socket incoming = s.accept();
+                    System.out.println("New Client Connected with id " 
++ i
++" from "+incoming.getInetAddress().getHostName()+"..." );
+                    Thread t = new ThreadedServer(incoming,i);
+                    i++;
+                    t.start();
+               }
+          }
+          catch(Exception e)
+          {
+               System.out.println("Error: " + e);
+          }
+     }
+}
+
+class ThreadedServer extends Thread
+{
+     int n;
+     String c,fn,fc;
+     String filenm;
+     Socket incoming;
+     int counter;
+     String dirn="c:/FTP SERVER DIRECTORY";
+     public ThreadedServer(Socket i,int c)
+     {
+          incoming=i;
+          counter=c;
+     }
+
+     public void run()
+     {
+          try
+          {
+
+               BufferedReader in =new BufferedReader(new
+InputStreamReader(incoming.getInputStream()));
+               PrintWriter out = new
+PrintWriter(incoming.getOutputStream(), true);
+               OutputStream output=incoming.getOutputStream();
+               fn=in.readLine();
+               c=fn.substring(0,1);
+
+               if(c.equals("#"))
+               {
+               n=fn.lastIndexOf("#");
+               filenm=fn.substring(1,n);
+               FileInputStream fis=null;
+               boolean filexists=true;
+               System.out.println("Request to download file "+filenm+"
+recieved from "+incoming.getInetAddress().getHostName()+"...");
+               try
+                 {
+                  fis=new FileInputStream(filenm);
+                 }
+               catch(FileNotFoundException exc)
+                 {
+                  filexists=false;
+                  System.out.println("FileNotFoundException:
+"+exc.getMessage());
+                 }
+                if(filexists)
+                {
+                 sendBytes(fis, output) ;
+		 fis.close();
+                }
+               }
+              else
+                 {
+                  try
+                  {
+                  boolean done=true;
+                  System.out.println("Request to upload file " +fn+"
+recieved from "+incoming.getInetAddress().getHostName()+"...");
+
+                  File dir=new File(dirn);
+                  if(!dir.exists())
+                  {
+                   dir.mkdir();
+                  }
+                  else
+                  {}
+                    File f=new File(dir,fn);
+                    FileOutputStream fos=new FileOutputStream(f);
+                    DataOutputStream dops=new DataOutputStream(fos);
+
+                   while(done)
+                   {
+                    fc=in.readLine();
+                    if(fc==null)
+                    {
+                     done=false;
+                    }
+                    else
+                    {
+                     dops.writeChars(fc);
+                    // System.out.println(fc);
+
+                    }
+                 }
+                 fos.close();
+                 }
+                 catch(Exception ecc)
+                 {
+                  System.out.println(ecc.getMessage());
+                 }
+                }
+               incoming.close();
+          }
+          catch(Exception e)
+          {
+               System.out.println("Error: " + e);
+          }
+     }
+     private static void sendBytes(FileInputStream f,OutputStream op)
+throws Exception
+     {
+      byte[] buffer=new byte[1024];
+      int bytes=0;
+
+      while((bytes=f.read(buffer))!=-1)
+      {
+       op.write(buffer,0,bytes);
+      }
+     }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+This is the implementation of the File Transfer Protocol in Java.The protocol is used for transfer of the file from remote system.
+
+To execute the program go through following steps:-
+
+1.install FtpServer.java on one machine.
+2.install FtpClient.java another machine.
+3.execute FtpServer.java.
+4.execute FtpClient.java.
+5.use GUI objects provided on the FtpClient.java to upload or download the particular file.
